132601e3f505f69014a89b80fb84f3d10e61771f
JiveCheng <zhesheng.zheng@gmail.com>
2023-10-21 13:56:56 +0800

    remove vendor path

--- vendor/codeception/codeception-progress-reporter/maketests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-for i in `seq 1 100`; do
-     php vendor/bin/codecept g:test unit Stub/Test$i
-done
-
-for i in `seq 101 200`; do
-     php vendor/bin/codecept g:test api Stub/Test$i
-done
